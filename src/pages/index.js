3417d1cdc88657b210b4c40f38a00ea1b2167307
cruz <cruz_ch96@outlook.com>
2021-08-19 13:29:27 +0800

    Initial Commit 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -138,12 +138,15 @@ const IndexPage = () => {
           🎉🎉🎉
         </span>
       </h1>
-      <p style={paragraphStyles}>
+      {/* <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
         update in real-time.{" "}
         <span role="img" aria-label="Sunglasses smiley emoji">
           😎
         </span>
+      </p> */}
+      <p style={paragraphStyles}>
+        Hello Andy Lee Sai Hong
       </p>
       <ul style={listStyles}>
         <li style={docLinkStyle}>
